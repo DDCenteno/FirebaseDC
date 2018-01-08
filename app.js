dc8c114744979e29bee1fc9a537675464ad2a132
DDCenteno <danielacentenoramos@gmail.com>
2018-01-08 12:42:07 -0500

    asdads

---
+++ app.js
@@ -0,0 +1,14 @@
+$('document').ready(function() {
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDj-LvKeMqL7iTWWUo8ki7cLGupd7GjFmk",
+    authDomain: "sociallab-a8547.firebaseapp.com",
+    databaseURL: "https://sociallab-a8547.firebaseio.com",
+    projectId: "sociallab-a8547",
+    storageBucket: "sociallab-a8547.appspot.com",
+    messagingSenderId: "973475069338"
+  };
+  firebase.initializeApp(config);
+  /* aca */
+
+});
